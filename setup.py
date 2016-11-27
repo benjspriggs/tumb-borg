261301c1b57f0f1afe86ce97eb94e3427cb038c9
Benjamin Spriggs <benjspriggs@sprico.com>
2016-11-26 23:31:02 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+setup(name='tumb-borg',
+        version='0.1',
+        author='Benjamin Spriggs',
+        author_email='benjspriggs@sprico.com',
+        license='Apache 3.0',
+        description='Simple poetry uploader.',
+        py_modules=['base'])
