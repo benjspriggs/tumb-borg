0bc802b8194f982642e9dca2e107dd19e9f29caa
Benjamin Spriggs <benjspriggs@sprico.com>
2016-11-29 15:26:49 -0800

    wrap lines, work with request urls, display in main

--- authorize.py
+++ authorize.py
@@ -1,23 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 from tumblpy import Tumblpy as T
+from urlparse import urlparse, parse_qs
 
 def authorize(KEY, SECRET, CALLBACK):
     def get_authorization_properties():
         t = T(KEY, SECRET)
-        return t.get_authentication_tokens(callback_url=CALLBACK)
+        return t
+            .get_authentication_tokens(
+                        callback_url=CALLBACK)
 
     auth_p = get_authorization_properties()
 
+    def get_auth_url():
+        print('Please connect with Tumblr via: \n%s' \
+                % auth_p['auth_url'])
+        result_url = \
+            raw_input("Copy and paste the accepting url: ") 
+        return result_url
+
+    def query_string(url):
+        return { k: v[0] for k, v in 
+                parse_qs(urlparse(url).query).items() }
+
+    def query_string_auth():
+        return query_string(get_auth_url())
+
     def authorized_tokens():
+        q = query_string_auth()
         t = T(KEY, SECRET,
-                auth_p['oauth_token'],
+                q['oauth_token'],
                 auth_p['oauth_token_secret'])
-        return t.get_authorized_tokens(auth_p['oauth_verifier'])
+        return t.get_authorized_tokens(q['oauth_verifier'])
 
     def authorized_t():
         a = authorized_tokens()
         return T(KEY, SECRET, 
                 a['oauth_token'], 
                 a['oauth_token_secret'])
-
+  
     return authorized_t()
--- tumb_borg.py
+++ tumb_borg.py
@@ -2,11 +2,16 @@
 
 from process import *
 from config import *
+from authorize import *
 from pprint import pprint
 
 if __name__ == "__main__":
-    print('Hello world!')
+    print('Poems are:')
     for poem in map(to_dictionary, generate_poems('example.txt')):
         print('Title is %s'% poem['title'])
         print('Tags are %s'% poem['tags'])
-    pprint(config())
+        # pprint(config())
+    # authorize
+    c = config()
+    auth = authorize(c['key'], c['secret'], c['callback'])
+    pprint(auth.post('user/info'))
