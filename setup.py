eeb12a83e510dbc68d9e448c22f0ed4aeeb83b11
Benjamin Spriggs <benjspriggs@sprico.com>
2016-12-05 21:31:14 -0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 setup(name='tumb-borg',
-    version='0.1',
+    version='0.2',
     author='Benjamin Spriggs',
     author_email='benjspriggs@sprico.com',
     license='Apache 3.0',
