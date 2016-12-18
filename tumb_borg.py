7c991de46028be573f79e75f9e2786a26f6b2cdc
Benjamin Spriggs <benjspriggs@sprico.com>
2016-12-18 13:57:07 -0800

    More closures and remove old global

--- tumb_borg.py
+++ tumb_borg.py
@@ -4,35 +4,9 @@ import sys, os
 from tumb_borg import *
 from pprint import pprint
 
-BATCH = "poem, poetry, spilled ink"
-
 def authorize_from_config(filename):
     c = config.app_config(filename)
     return authorize.authorize(c['key'], c['secret'], c['callback'])
-
-def get_batch(filename):
-    c = config.app_config(filename)
-    return c['batch-tags']
-
-def post_poems(auth,    \
-        ident,          \
-        poem_generator, \
-        BATCH="poem, poetry, spilled ink"):
-    def queue_text_post(payload):
-        return { 'type': 'text',
-                'state': 'queue',
-                'tags':  ', '.join(payload['tags']) + ", " + BATCH,
-                'body':  '\n'.join(payload['content']),
-                'title': '// %s' % payload['title'] }
-    for poem in (queue_text_post(p) for p in poem_generator):
-        print(poem)
-        print(auth.post('blog/%s/post' % ident, \
-                params=poem))
-
-def poem_dicts(filename):
-    return (process.to_dictionary(poem) \
-            for poem \
-            in process.generate_poems(filename))
 
 def usage():
     print('usage: ./tumb_borg.py <blogname> <filename> [<config-filename>]')
@@ -45,23 +19,45 @@ def validate_arguments(argv):
 
 # ./tumb_borg.py <blogname> <filename> [<config-filename>]
 def batch_post_poems(argv):
-    blogname = sys.argv[1]
-    filename = sys.argv[2]
+    blogname = argv[1]
+    filename = argv[2]
     setting  = None
 
     # authorize
-    if len(sys.argv) >= 4:
-        setting = sys.argv[3]
+    if len(argv) >= 4:
+        setting = argv[3]
     else:
         setting = 'app.secret.yml'
 
-    batch = get_batch(os.path.realpath(setting))
+    def get_batch_tags(filename):
+        c = config.app_config(filename)
+        return c['batch-tags']
+
+    batch = get_batch_tags(os.path.realpath(setting))
     auth  = authorize_from_config( \
             os.path.realpath(setting))
     # store the key for now TODO: implement
     # store(app_config())
     # check that the user can post to this blog TODO: implement
     # post poems
+    def post_poems(auth,    \
+            ident,          \
+            poem_generator, \
+            BATCH="poem, poetry, spilled ink"):
+        def queue_text_post(payload):
+            return { 'type': 'text',
+                    'state': 'queue',
+                    'tags':  ', '.join(payload['tags']) + ", " + BATCH,
+                    'body':  '\n'.join(payload['content']),
+                    'title': '// %s' % payload['title'] }
+        for poem in (queue_text_post(p) for p in poem_generator):
+            print(poem)
+            print(auth.post('blog/%s/post' % ident, \
+                    params=poem))
+        def poem_dicts(filename):
+            return (process.to_dictionary(poem) \
+                    for poem \
+                    in process.generate_poems(filename))
     post_poems(auth, \
             blogname, \
             poem_dicts(filename), \
