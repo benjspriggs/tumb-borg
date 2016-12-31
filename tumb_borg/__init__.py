c46ae4a72d3ce5418ca8a9baa994c3bec8134f55
Benjamin Spriggs <benjspriggs@sprico.com>
2016-12-30 23:24:36 -0800

    rename source dir

---
+++ tumb_borg/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['authorize',
+    'config',
+    'interactive',
+    'process']
---
+++ tumb_borg/interactive.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+_POEM_FORMAT_STRING="// %s\n%s\n#%s\n\n"
+
+def print_poem(poem):
+    print('Title: \t%s' % poem['title'])
+    print('Tags:\t%s' % ", ".join(poem['tags']))
+
+def print_poem_full(poem):
+    print_poem(poem)
+    print('Body:\r%s' % "\n".join(poem['content']))
+
+def save_poems(filename, poems):
+    with open(filename, 'w') as f:
+        def save_poem(of, poems):
+            of.write( _POEM_FORMAT_STRING % \
+                    (poem['title'], \
+                    "\n".join(poem['content']), \
+                    " #".join(poem['tags']))
+                    )
+
+        for poem in poems:
+            save_poem(f, poem)
