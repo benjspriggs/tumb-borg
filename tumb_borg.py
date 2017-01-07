1de411a05422281f02333883d6af4c632789f25f
Benjamin Spriggs <benjspriggs@sprico.com>
2017-01-07 14:20:45 -0800

    add debug

--- tumb_borg.py
+++ tumb_borg.py
@@ -40,6 +40,7 @@ def batch_post_poems(blogname, filename, setting):
     def authorize_from_config():
         def renew_tokens():
             a = authorize.authorize(c['key'], c['secret'], c['callback'])
+            pprint(a)
             a_t = authorize.authorized_t(a['key'], a['secret'], a)
             c['oauth_token'] = a_t.oauth_token
             c['oauth_token_secret'] = a_t.oauth_token_secret
